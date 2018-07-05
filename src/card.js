f8213ce7085ade2c8b15d54f49c3c46934be6499
Lorraine Odijk <lorrainesheelah@users.noreply.github.com>
2018-07-05 16:22:55 +0200

    bug fixed

--- src/card.js
+++ src/card.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Card = ({name, email, id}) => {
+const Card = ({ name, email, id }) => {
 	
 	return (
 		<div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow=5'>
-			<img alt='robots' src={'https://robohash.org/${props.id}?set=set3?200x200?bgset=bg1'} />
+			<img alt='robots' src={`https://robohash.org/${id}?200x200`} />
 			<div>
 				<h2>{name}</h2>
 				<p>{email}</p>
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js'
+import card from './card.js';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
+import robots from './robots.js';
 
 ReactDOM.render( <App/>, document.getElementById('root'));
 registerServiceWorker();
