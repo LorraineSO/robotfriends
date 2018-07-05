64e38aa78b29b9a68cdcbdbaa3dcfcd065062783
Lorraine Odijk <lorrainesheelah@users.noreply.github.com>
2018-07-05 17:29:32 +0200

    final in theory

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './cardList.js';
+import SearchBox from './SearchBox.js';
 import  { robots } from './robots.js';
+import './App.css';
 
-const App = () => {
-	return (
-			<CardList robots={robots}/>
-		)
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			robots: robots,
+			searchfield: ''
+		}
+	}
+
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value })
+		
+	}
+	render () {
+		const filteredRobots = this.state.robots.filter(robots =>{
+			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		})
+		return (
+			<div className='tc'>
+			<h1 className='f1'>R-friends</h1>
+			<SearchBox searchChange={this.onSearchChange}/>
+			<CardList robots={filteredRobots}/>
+			</div>
+			);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js'
-import card from './card.js';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
-import robots from './robots.js';
 
 ReactDOM.render( <App/>, document.getElementById('root'));
 registerServiceWorker();
